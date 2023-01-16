74b886272b20e94450253f15a1d1cdc495513e1f
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-01-16 19:54:59 +0300

    Wrote the Data layer

---
+++ app/src/main/java/com/vlyashuk/numbercomposition/data/GameRepositoryImpl.kt
@@ -0,0 +1,67 @@
+package com.vlyashuk.numbercomposition.data
+
+import com.vlyashuk.numbercomposition.domain.entity.GameSettings
+import com.vlyashuk.numbercomposition.domain.entity.Level
+import com.vlyashuk.numbercomposition.domain.entity.Question
+import com.vlyashuk.numbercomposition.domain.repository.GameRepository
+import kotlin.math.max
+import kotlin.math.min
+
+import kotlin.random.Random
+
+object GameRepositoryImpl : GameRepository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 1
+
+    override fun generateQuestion(maxSumValue: Int, countOfOptions: Int): Question {
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxSumValue + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer - countOfOptions, MIN_ANSWER_VALUE)
+        val to = min(maxSumValue, rightAnswer + countOfOptions)
+        while (options.size < countOfOptions) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Question(sum, visibleNumber, options.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        return when (level) {
+            Level.TEST -> {
+                GameSettings(
+                    10,
+                    2,
+                    50,
+                    10
+                )
+            }
+            Level.EASY -> {
+                GameSettings(
+                    10,
+                    10,
+                    50,
+                    60
+                )
+            }
+            Level.NORMAL -> {
+                GameSettings(
+                    20,
+                    20,
+                    70,
+                    50
+                )
+            }
+            Level.HARD -> {
+                GameSettings(
+                    30,
+                    30,
+                    90,
+                    40
+                )
+            }
+        }
+    }
+}
