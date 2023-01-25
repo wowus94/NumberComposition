a4630bb4f9be3888c2df8bb2320816175fe0defc
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-01-25 19:51:22 +0300

    Add Jetpack Navigation

--- app/src/main/java/com/vlyashuk/numbercomposition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/vlyashuk/numbercomposition/presentation/ChooseLevelFragment.kt
@@ -44,7 +44,7 @@ class ChooseLevelFragment : Fragment() {
     private fun launchGameFragment(level: Level) {
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(null)
+            .addToBackStack(GameFragment.NAME )
             .commit()
     }
 
